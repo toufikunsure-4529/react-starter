d9911da2b97ce07cce977cf02aefa00937da24a1
TOUFIK SK <toufiksk.ab3@gmail.com>
2024-02-08 00:29:27 +0530

    Simple Project Make Using React useState hooks concepts background-color-chnager

---
+++ bgchanger-app/src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+
+function App() {
+  const [color, setColor] = useState("olive");
+  // onClick={setColor("blue")} we never write this type onclick under function return excute because onclick methood expect only function not funtion return value its problem js syntax issue we cannnot pass on click function under parameters so we used onclick under callback and setColor function run
+  return (
+    <div
+      className="w-full h-screen duration-200"
+      style={{ backgroundColor: color }}
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button
+            onClick={() => setColor("red")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "red" }}
+          >
+            Red
+          </button>
+          <button
+            onClick={() => setColor("green")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "green" }}
+          >
+            Green
+          </button>
+          <button
+            onClick={() => setColor("yellow")}
+            className="outline-none px-4 py-1 rounded-full text-black shadow-lg"
+            style={{ backgroundColor: "yellow" }}
+          >
+            Yellow
+          </button>
+          <button
+            onClick={() => setColor("black")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "black" }}
+          >
+            Black
+          </button>
+          <button
+            onClick={() => setColor("blue")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "blue" }}
+          >
+            Blue
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
