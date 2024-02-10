664ca4946cbe78d22d9b7e3316bd1d8149bb09f7
TOUFIK SK <toufiksk.ab3@gmail.com>
2024-02-11 01:16:09 +0530

    Create simple Password Generator App using React and learn useCallback, useEffect, useRef, useState  Hooks in depth ongoing this project

--- passwordGenerator-app/src/App.jsx
+++ passwordGenerator-app/src/App.jsx
@@ -1,19 +1,110 @@
-import { useState } from 'react'
-import './App.css'
+import { useCallback, useEffect, useRef, useState } from "react";
+import "./App.css";
+import Toast from "./Toast";
 
 function App() {
-  const [length,setLength]=useState(8)
-  const [numberAllowed,setNumberAllowed]=useState(false)
-  const [charAllowed,setCharAllowed]=useState(false)
-  const [password,setPassword]=useState("")
-  
-  const passwordGenrator=()=>{}
+  const [length, setLenghth] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+  const [showToast, setShowToast] = useState(false);
+
+  //user useRef hooks
+  const passwordRef = useRef(null);
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXZabcdefghijklmnopqrstuvwxyz";
+    
+    if (numberAllowed) {
+      str += "0123456789";
+    }
+    if (charAllowed) {
+      str += "!@#$%^&*()_";
+    }
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char); //charAt perticular index to character retarive
+    }
+
+    setPassword(pass);
+  }, [length, numberAllowed, charAllowed, setPassword]);
+
+
+  //copy password methood
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, length);
+    window.navigator.clipboard.writeText(password);
+    setShowToast(true)
+  }, [password]);
+
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, numberAllowed, charAllowed, passwordGenerator]);
+
   return (
     <>
-      <h1 className='text-4xl text-center text-white'>Password Generator</h1>
- 
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-700">
+        <h1 className="text-white text-center my-3">Password Generator</h1>
+        <div className="flex shadow-md rounded-lg overflow-hidden md-4">
+          <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="Password"
+            readOnly
+            ref={passwordRef}
+          />
+          <button
+            className="outline-none bg-blue-600 text-white px-3 py-0.5 shrink-0"
+            onClick={copyPasswordToClipboard}
+          >
+            Copy
+          </button>
+        </div>
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-1">
+            <input
+              type="range"
+              min={6}
+              max={100}
+              value={length}
+              className="cursor-pointer"
+              onChange={(e) => {
+                setLenghth(e.target.value);
+              }}
+            />
+            <label htmlFor="length">Length: {length}</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={numberAllowed}
+              id="numberInput"
+              onChange={() => {
+                setNumberAllowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor="numberInput">Number</label>
+          </div>
+
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={charAllowed}
+              id="charInput"
+              onChange={() => {
+                setCharAllowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor="charInput">Charaters</label>
+          </div>
+        </div>
+      </div>
+      {showToast && <Toast />}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
