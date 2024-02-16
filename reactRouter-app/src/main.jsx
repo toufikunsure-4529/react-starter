0b7584319c13bfd9d9fdc2c00ea354aebdbcba3e
TOUFIK SK <toufiksk.ab3@gmail.com>
2024-02-16 23:58:22 +0530

    Bug fix routing page

--- reactRouter-app/src/main.jsx
+++ reactRouter-app/src/main.jsx
@@ -1,29 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'; //RouterProvider props pass this provider means router props pass and when router create to import createBrowserRouter
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Layout from './Layout';
-import About from './components/About/About';
-import Home from './components/Home/Home';
+import About from './components/About/About.jsx';
+import Home from './components/Home/Home.jsx';
 import './index.css';
 
-const router=createBrowserRouter([ //createBrowserRouter method pass array under pass router object 
+//Create router
+const router=createBrowserRouter([
   {
-    path:'/', //which path to view index path normally view
-    element:<Layout />, //element Layout view means Header Oultlet Footer
-    children:[  //Layout to another link children add path wise
+    path:'/', //path / menad it is top level element in this under nesting all page about contact home so on <Header> Footer load 
+    element:<Layout />,  //which component layout base render this component element define Header/footer/outlen-method
+    children:[
       {
         path:'',
-        element:<Home /> //Home Path of router
+        element: <Home />
       },{
-        path:"about", //this is /about path
-        element:<About/>
+        path:'about',
+        element:<About />
       }
-    ]
+    ] //children add for Home about contact  page nesting routing
   }
-])
+])  //createBrowserRouter it is method refer react router import it work browrser router cretae responsbitliy
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={router} /> {/* this provider create a router pass by router as props and this provider react router dom concept */}
   </React.StrictMode>,
 )
